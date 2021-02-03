e657eb28abb18ae316ff0e23da5d55c07acc9a3b
Gr1zzlyBear <ray.rahat@melodyvr.com>
2021-02-03 17:39:46 +0000

    my first framework

---
+++ features/step-definitions/steps.js
@@ -0,0 +1,14 @@
+import {Given, When, Then} from "cucumber";
+
+When(/^I open google$/, ( ) => {
+
+}
+    
+    );
+
+
+    Then(/^I can see the google logo$/, ( ) => {
+
+    }
+        
+        );
---
+++ features/test.feature
@@ -0,0 +1,12 @@
+Feature: Test Feature 
+
+  My first test Feature
+
+
+  Scenario: Test number 1
+  
+  When I open google
+  Then I can see the google logo
+
+  
+   
